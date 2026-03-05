fd62a0a8a99f4635df927e674e07124f7d22b7de
julianicioli <julianicioli92@gmail.com>
2026-03-05 16:14:24 -0300

    tela cadastro funcionario

---
+++ php/cadastrofuncionario.php
@@ -0,0 +1,19 @@
+<?php
+    $nome = $_POST['nome'];
+    $rg = $_POST['rg'];
+    $cpf = $_POST['cpf'];  
+    $numero_registro = $_POST['numero_registro'];
+    $cargo = $_POST['cargo'];
+    $senha = $_POST['senha'];
+    $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
+
+    require_once('conexao.php');
+
+    $bancodedados = new db();
+
+    $link = $bancodedados->conecta_mysql();
+
+    $sql = "insert into cadastrofuncionario(nome, rg, cpf, numero_registro, cargo, senha) values('$nome', '$rg', '$cpf', '$numero_registro', '$cargo', '$senha')";
+
+    mysqli_query($link, $sql);
+?>
